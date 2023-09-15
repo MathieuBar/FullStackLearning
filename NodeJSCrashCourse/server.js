4dcd4ccff76417ab88ce2d75f5679022dea63e15
Mathieu Barthès <mathieu.barthes@xelya.com>
2023-09-15 15:23:25 +0200

    Part 4 - 1/2 : request and responses

--- NodeJSCrashCourse/server.js
+++ NodeJSCrashCourse/server.js
@@ -1,7 +1,20 @@
 const http = module.require('http')
+const fs = module.require('fs')
 
 const server = http.createServer((req, res) => {
     console.log('Request received')
+    console.log(req.url, req.method)
+
+    res.setHeader('Content-Type', 'text/html')
+
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.end(data)
+        }
+    })
 })
 
 server.listen(3000, 'localhost', () => {
