913928ff37e20a43e15210e46b56474ef4817bf9
Mathieu Barthès <mathieu.barthes@xelya.com>
2023-09-20 15:32:08 +0200

    NodeJS crash course 5 - Express basics

---
+++ NodeJSCrashCourse/app.js
@@ -0,0 +1,23 @@
+const express = module.require('express')
+const app = express()
+
+app.listen(3000)
+
+app.get('/', (req, res) => {
+    // res.send('<p>Home page</p>')
+    res.sendFile('./views/index.html', { root: __dirname })
+})
+
+app.get('/about', (req, res) => {
+    // res.send('<p>About page</p>')
+    res.sendFile('./views/about.html', { root: __dirname })
+})
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
+
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname })
+})
+
