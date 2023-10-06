f68f085300e77a0401c6326c983493538e67cfe8
Mathieu Barthès <mathieu.barthes@xelya.com>
2023-10-06 11:42:59 +0200

    NodeJS crash course 9 - MongoDB

--- NodeJSCrashCourse/app.js
+++ NodeJSCrashCourse/app.js
@@ -1,34 +1,52 @@
+const fs = module.require('fs');
 const express = module.require('express');
 const morgan = module.require('morgan');
+const Blog = module.require('./models/Blog');
+const mongoose = module.require('mongoose');
 
 // express app
 const app = express();
 
+// db connexion
+fs.readFile('./resources/connexionString', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const connexionString = data.toString();
+    mongoose.connect(connexionString)
+      .then(app.listen(3000, () => console.log('server listening to port 3000')))
+      .catch(err => console.log(err))
+      ;
+  }
+});
+
 // listen for requests
-app.listen(3000);
 
 // register view engine
 app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
 
-// logger middleware (3rd party middelware "morgan")
+// middleware : logger 'morgan' and static pages
 app.use(morgan('dev'));
-
-// static pages middleware (built-in express static middleware)
 app.use(express.static('public'));
 
+// routes
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { title: 'About' });
 });
+
+// blog routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1})
+    .then(result => res.render('index', {
+      title: 'All blogs',
+      blogs: result,
+    }))
+    .catch(err => console.log(err))
+})
 
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
