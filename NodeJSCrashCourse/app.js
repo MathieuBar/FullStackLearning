4e9ca613048a84237eff4e15d04b542022010820
Mathieu Barthès <mathieu.barthes@xelya.com>
2023-10-13 09:16:02 +0200

    NodeJSCrashCourse 10 : get, post & delete requests

--- NodeJSCrashCourse/app.js
+++ NodeJSCrashCourse/app.js
@@ -14,20 +14,19 @@ fs.readFile('./resources/connexionString', (err, data) => {
   } else {
     const connexionString = data.toString();
     mongoose.connect(connexionString)
-      .then(app.listen(3000, () => console.log('server listening to port 3000')))
+      .then(() => app.listen(3000, () => console.log('server listening to port 3000')))
       .catch(err => console.log(err))
       ;
   }
 });
 
-// listen for requests
-
 // register view engine
 app.set('view engine', 'ejs');
 
-// middleware : logger 'morgan' and static pages
+// middleware : logger 'morgan', static pages, urlencoder
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.get('/', (req, res) => {
@@ -40,7 +39,7 @@ app.get('/about', (req, res) => {
 
 // blog routes
 app.get('/blogs', (req, res) => {
-  Blog.find().sort({ createdAt: -1})
+  Blog.find().sort({ createdAt: -1 })
     .then(result => res.render('index', {
       title: 'All blogs',
       blogs: result,
@@ -51,6 +50,34 @@ app.get('/blogs', (req, res) => {
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
 });
+
+app.post('/blogs', (req, res) => {
+  const formContent = req.body;
+  const blog = new Blog(formContent) // todo : ensure input data
+  blog.save()
+    .then((result) => res.redirect('/blogs'))
+    .catch((err) => console.log(err))
+    ;
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => res.render('details', {
+      title: `Post | ${result.title}`,
+      blog: result,
+    }))
+    .catch((err) => console.log(err))
+})
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({redirect: '/blogs'})
+    })
+})
+
 
 // 404 page
 app.use((req, res) => {
